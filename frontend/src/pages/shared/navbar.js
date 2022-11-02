fd556eeea0b2aca1c89a22987b21fbbfb9ca19ce
andersonPaez <anderson.paez.1412@gmail.com>
2022-11-02 00:08:48 -0500

    modificacion <aside>

--- frontend/src/pages/shared/navbar.js
+++ frontend/src/pages/shared/navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     <nav className="main-header navbar navbar-expand navbar-white navbar-light">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a classname="nav-link" data-widget="pushmenu" href="#" role="button"><i className="fas fa-bars"/></a>
+          <a classname="nav-link" data-widget="pushmenu" href="#" role="button"><i className="fas fa-bars"></i></a>
         </li>
         <li className="nav-item d-none d-sm-inline-block">
           <a className="nav-link">Inicio</a>
--- frontend/src/pages/shared/sidebar.js
+++ frontend/src/pages/shared/sidebar.js
@@ -1,18 +1,67 @@
-import React from 'react'
-import logosimple from "../../assets/image/logosimple.png";
+import {React, useState} from "react";
+import {Link} from "react-router-dom";
+import iconAdmin from "../../assets/icons/user-tie-solid.svg";
 
 export default function Sidebar() {
+
+  const [itemUsuarioOpen, setItemUsuarioOpen] = useState(true);
+
   return (
-    <aside className="main-sidebar sidebar-dark-primary elevation-4">
-      <a href="#" className="brand-link">
-        <img
-          src={logosimple}
-          alt="AdminService Logo"
-          className="brand-image img-circle elevation-3"
-          style={{opacity: ".8"}} />
+    <aside className="main-sidebar sidebar-dark-primary elevation-4 altura" style={{height:"100vh"}}>
+      <Link to="/" className="brand-link">
         <span className="brand-text font-weight-light">AdminService</span>
-      </a>
-      
+      </Link>
+      <div>
+        <div className="sidebar">
+          <div className="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div className="image">
+              <img
+                src={iconAdmin}
+                className="img-circle elevation-2"
+                alt="iconoAdmin"
+              />
+            </div>
+            <div className="info">
+              <Link>
+                Administrador
+              </Link>
+            </div>
+          </div>
+        </div>
+        <nav>
+          <ul
+            className="nav nav-pills nav-sidebar flex-column"
+            data-widget="treeview"
+            role="menu"
+            data-accordion="false">
+            <li className={ "nav-item" + (itemUsuarioOpen ? " menu-open": "" ) } >
+              <div className="nav-link" onClick={ ()=>{
+                setItemUsuarioOpen( !itemUsuarioOpen )
+              } }>
+                <i className="nav-icon fas fa-tachometer-alt" />
+                <p>
+                  Usuarios
+                  <i className="right fas fa-angle-left" />
+                </p>
+              </div>
+              <ul className="nav nav-treeview">
+                <li className="nav-item">
+                  <a href="#" className="nav-link">
+                    <i className="far fa-circle nav-icon" />
+                    <p>Listar</p>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="#" className="nav-link">
+                    <i className="far fa-circle nav-icon" />
+                    <p>Crear</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </aside>
   );
 }
